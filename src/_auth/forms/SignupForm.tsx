711b48a799c71e85467e29fe0a706a2995a572a0
manuel-heav <manuelheav32@gmail.com>
2023-10-29 18:08:22 +0300

    working on sign up form

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -12,52 +12,56 @@ import { Input } from "@/components/ui/input"
 import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
+import { SignupValidation } from "@/lib/validation"
  
 
-const formSchema = z.object({
-  username: z.string().min(2).max(50),
-})
+
 export default function SignupForm() {
 
   // 1. Define your form.
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof SignupValidation>>({
+    resolver: zodResolver(SignupValidation),
     defaultValues: {
+      name: "",
+      email: "",
+      password: "",
       username: "",
     },
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof SignupValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values)
   }
 
   return (
-    <div>
       <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
+
+        <div className="sm:w-420 flex-center flex-col">
+            <img src="/assets/images/logo.png" className="h-40" />
+        </div>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
     </Form>
-    </div>
   )
 }
 
--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -2,5 +2,8 @@ import * as z from "zod"
  
 
 export const SignupValidation = z.object({
-    username: z.string().min(2).max(50),
+    name: z.string().min(2, {message: "That all you got. Come on."}).max(50),
+    username: z.string().min(2, {message: "Provide a longer username please."}).max(50),
+    email: z.string().email(),
+    password: z.string().min(8, {message: "Longer(password) is always better, THAT'S WHAT SHE SAID!"}),
   })
