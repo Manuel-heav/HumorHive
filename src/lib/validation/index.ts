b4ec349785c887db4eca0b4e9755231a803fc66d
manuel-heav <manuelheav32@gmail.com>
2023-10-31 20:10:53 +0300

    adjusted validation

--- src/lib/validation/index.ts
+++ src/lib/validation/index.ts
@@ -2,8 +2,8 @@ import * as z from "zod"
  
 
 export const SignupValidation = z.object({
-    name: z.string().min(2, {message: "That all you got. Come on."}).max(50),
-    username: z.string().min(2, {message: "Provide a longer username please."}).max(50),
+    name: z.string().min(2, {message: "That all you got? Come on."}).max(50, {message: "Too much bro."}),
+    username: z.string().min(2, {message: "Provide a longer username please."}).max(50, {message: "Too much bro."}),
     email: z.string().email(),
-    password: z.string().min(8, {message: "Longer(password) is always better, THAT'S WHAT SHE SAID!"}),
+    password: z.string().min(8, {message: "Longer is always better, THAT'S WHAT SHE SAID!"}),
   })
