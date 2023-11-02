a930d0d1b383a6bc53d0960d1a327e26dc2415b4
manuel-heav <manuelheav32@gmail.com>
2023-11-02 21:09:39 +0300

    working on topbar

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import { Button } from '../ui/button'
 
 const Topbar = () => {
   return (
-    <div>Topbar</div>
+    <section className='topbar'>
+        <div className='flex-between py-4 px-5'>
+            <Link to="/" className="flex gap-3 items-center">
+                <img src="/assets/images/logo.png" className="-mt-6" width={120} height={110} />
+            </Link>
+
+
+            <div className='flex gap-4'>
+                <Button variant="ghost" className='shad-button_ghost' onClick={signOut}>
+                    <img src="/assets/icons/logout.svg" alt="" />
+                </Button>
+            </div>
+        </div>
+    </section>
   )
 }
 
