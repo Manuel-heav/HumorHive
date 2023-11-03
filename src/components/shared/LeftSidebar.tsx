defeaaa1937f6425aa604e65563a1cdfec6bdb9e
manuel-heav <manuelheav32@gmail.com>
2023-11-03 11:33:33 +0300

    working on sidebar

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -25,8 +25,24 @@ const LeftSidebar = () => {
             </Link>
 
             <Link to={`/profile/${user.id}`} className='flex gap-3 items-center'>
-              <img src={user.imageUrl || "/assets/icons/profile-placeholder.svg"} alt="" className='h-14 w-14 rounded-full' />
+              <img src={user.imageUrl || "/assets/icons/profile-placeholder.svg"} alt="" className='h-10 w-10 rounded-full' />
+
+              <div className='flex flex-col'>
+                <p className='body-bold'>{user.name}</p>
+                <p className='small-regular text-light-3'>@{user.username}</p>
+              </div>
             </Link>
+
+
+            <ul className='flex flex-col gap-6'>
+              <Link></Link>
+              <Link></Link>
+              <Link></Link>
+              <Link></Link>
+              <Link></Link>
+              <Link></Link>
+              <Link></Link>
+            </ul>
         </div>
     </nav>
   )
