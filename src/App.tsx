4aed8bd2e203dcaeb150dfa9529e79af3c6eb816
manuel-heav <manuelheav32@gmail.com>
2023-11-06 10:44:02 +0300

    working on other routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,14 @@ import { Home } from './_root/pages'
 import AuthLayout from './_auth/AuthLayout'
 import RootLayout from './_root/RootLayout'
 import { Toaster } from "@/components/ui/toaster"
+import Explore from './_root/pages/Explore';
+import Saved from './_root/pages/Saved';
+import AllUsers from './_root/pages/AllUsers';
+import CreatePost from './_root/pages/CreatePost';
+import EditPost from './_root/pages/EditPost';
+import PostDetails from './_root/pages/PostDetails';
+import Profile from './_root/pages/Profile';
+import UpdateProfile from './_root/pages/UpdateProfile';
 
 const App = () => {
   return (
@@ -23,6 +31,21 @@ const App = () => {
           {/* Only Logged in Users see */}
           <Route element={<RootLayout />}>
               <Route index element={<Home />}/>
+              <Route path="/explore" element={<Explore />}/>
+              <Route path="/saved" element={<Saved />}/>
+
+              <Route path="/all-users" element={<AllUsers />}/>
+
+              <Route path="/create-post" element={<CreatePost />}/>
+
+              <Route path="/update-post/:id" element={<EditPost />}/>
+
+              <Route path="/posts/:id" element={<PostDetails />}/>
+
+              <Route path="/profile/:id/*" element={<Profile />}/>
+
+              <Route path="/update-profile/:id" element={<UpdateProfile />}/>
+
           </Route>
 
         </Routes>
