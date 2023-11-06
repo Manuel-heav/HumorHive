23e0a68459d8d69c85e8a8a1edb4e116da0bdbd8
manuel-heav <manuelheav32@gmail.com>
2023-11-06 10:36:55 +0300

    working on sidebar

--- src/components/shared/LeftSidebar.tsx
+++ src/components/shared/LeftSidebar.tsx
@@ -27,7 +27,7 @@ const LeftSidebar = () => {
             </Link>
 
             <Link to={`/profile/${user.id}`} className='flex gap-3 items-center'>
-              <img src={user.imageUrl || "/assets/icons/profile-placeholder.svg"} alt="" className='h-10 w-10 rounded-full' />
+              <img src={user.imageUrl || "/assets/icons/profile-placeholder.svg"} alt="" className='h-8 w-8 rounded-full' />
 
               <div className='flex flex-col'>
                 <p className='body-bold'>{user.name}</p>
@@ -36,13 +36,13 @@ const LeftSidebar = () => {
             </Link>
 
 
-            <ul className='flex flex-col gap-6'>
+            <ul className='flex flex-col gap-2'>
              {sidebarLinks.map((link: INavLink) => {
 
               const isActive = pathname === link.route
               return (
-                <li key={link.label} className={`leftsidebar-link ${isActive && 'bg-primary-500'}`}>
-                    <NavLink to={link.route} className="flex gap-4 items-center p-4">
+                <li key={link.label} className={`leftsidebar-link group ${isActive && 'bg-primary-500'}`}>
+                    <NavLink to={link.route} className="flex gap-4 items-center p-3">
                     <img src={link.imgURL} alt={link.label} className={`group-hover:invert-white ${isActive && 'invert-white'}`}/>
                   {link.label}
                 </NavLink>
@@ -51,6 +51,13 @@ const LeftSidebar = () => {
              })}
             </ul>
         </div>
+
+        <Button 
+        variant="ghost"    
+        className='shad-button_ghost' onClick={()=> signOut()}>
+                    <img src="/assets/icons/logout.svg" alt="" />
+                    <p className='small-medium lg:base-medium'>Logout</p>
+                </Button>
     </nav>
   )
 }
