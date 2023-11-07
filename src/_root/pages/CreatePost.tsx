4a2559c48be7e9c1eeebab4429a2ed44de233299
manuel-heav <manuelheav32@gmail.com>
2023-11-07 14:36:12 +0300

    working with the form of create post section

--- src/_root/pages/CreatePost.tsx
+++ src/_root/pages/CreatePost.tsx
@@ -1,3 +1,4 @@
+import PostForm from '@/components/forms/PostForm'
 import React from 'react'
 
 const CreatePost = () => {
@@ -9,6 +10,8 @@ const CreatePost = () => {
 
           <h2 className='h3-bolc md:h2-bold text-left w-full'>Create Post</h2>
         </div>
+
+        <PostForm />
       </div>
     </div>
   )
