d265a18a627bf31ec8f6b1304ac7fbd54a0df421
manuel-heav <manuelheav32@gmail.com>
2023-11-08 19:22:57 +0300

    working on home page

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,8 +1,16 @@
-import React from 'react'
-
 const Home = () => {
+  const isPostLoading = true;
+  const posts = null;
+  
   return (
-    <div>Home</div>
+    <div className="flex flex-1">
+      <div className="home-container">
+        <div className="home-posts">
+          <h2 className="h3-bold md-h2-bold text-left w-full">Home Feed</h2>
+          {isPostLoading && !posts}
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,7 +1,8 @@
 import {useQuery, useMutation, useQueryClient, useInfiniteQuery} from "@tanstack/react-query";
-import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
+import { createPost, createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
 import { INewPost, INewUser } from "@/types";
 import CreatePost from "@/_root/pages/CreatePost";
+import { QUERY_KEYS } from "./queryKeys";
 
 
 export const useCreateUserAccount = () => {
@@ -26,5 +27,14 @@ export const useSignOutAccount = () => {
 }
 
 export const useCreatePost = () => {
-    
+    const queryClient = useQueryClient();
+    return useMutation({
+        mutationFn: (post: INewPost) => createPost(post),
+
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
+            })
+        }
+    })
   };
