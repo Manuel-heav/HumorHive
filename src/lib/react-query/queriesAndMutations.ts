5d77f71025f4a6935631fc59ee20d4f21f300ad6
manuel-heav <manuelheav32@gmail.com>
2023-11-08 19:06:24 +0300

    working on useCreatePost mutation

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,6 +1,7 @@
 import {useQuery, useMutation, useQueryClient, useInfiniteQuery} from "@tanstack/react-query";
 import { createUserAccount, signInAccount, signOutAccount } from "../appwrite/api";
-import { INewUser } from "@/types";
+import { INewPost, INewUser } from "@/types";
+import CreatePost from "@/_root/pages/CreatePost";
 
 
 export const useCreateUserAccount = () => {
@@ -23,3 +24,7 @@ export const useSignOutAccount = () => {
         mutationFn: signOutAccount
     })
 }
+
+export const useCreatePost = () => {
+    
+  };
