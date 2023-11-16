82aeabbd063ccb067af18124798f18967f6ddc57
manuel-heav <manuelheav32@gmail.com>
2023-11-16 20:45:51 +0300

    working on post details apge

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,8 +1,42 @@
-import React from 'react'
-
+import Loader from "@/components/shared/Loader";
+import { useUserContext } from "@/context/AuthContext";
+import { useGetPostById } from "@/lib/react-query/queriesAndMutations"
+import { timeAgo } from "@/lib/utils";
+import { Link, useParams } from "react-router-dom";
 const PostDetails = () => {
+  const { id } = useParams()
+  const {data: post, isPending } = useGetPostById(id || '');
+  const { user } = useUserContext();
   return (
-    <div>PostDetails</div>
+    <div className="post_details-container">
+      {isPending ? <Loader /> : (
+        <div className="post_details-card">
+          <img src={post?.imageUrl} className="post_details-img" alt="" />
+          <div className="post_details-info">
+            <div className="flex-between w-full">
+            <Link to={`/profile/${post?.creator.$id}`} className="fkex items-center gap-3">
+                    <img src={post?.creator?.imageUrl || '/assets/icons/profile-placeholder.svg'} alt="Creator" className='rounded-full w-8 h-8 lg:h-10 lg:w-10' />
+
+                <div className="flex flex-col">
+                    <p className='base-medium lg:body-bold text-light-1'>{post?.creator.name}</p>
+
+                    <div className='flex-center gap-2 text-light-3'>
+                        <p className='subtle-semibold lg:small-regular'>{timeAgo(post?.$createdAt)}</p> -
+                        <p className='subtle-semibold lg:small-regular'>{post?.location}</p>
+                    </div>
+                </div>
+                </Link>
+
+                <div className="flex-center gap-4">
+                  <Link to={`/update-post/${post?.$id}`} className={`${user.id !== post?.creator.$id && 'hidden'}`}>
+                    <img src="/assets/icons/edit.svg" alt="" width={24} height={24}/>
+                  </Link>
+                </div>
+            </div>
+            </div>
+        </div>
+      )}
+    </div>
   )
 }
 
