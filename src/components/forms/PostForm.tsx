21bcb02824146563221bad97ee09c480014ed9de
manuel-heav <manuelheav32@gmail.com>
2023-11-16 20:31:17 +0300

    working on post details apge

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -139,7 +139,7 @@ const PostForm = ({ post, action }: PostFormProps) => {
         />
         <div className="flex gap-4 items-center justify-end">
             <Button type="button" className="shad-button_dark_4">Cancel</Button>
-            <Button type="submit" className="shad-button_primary whitespace-nowrap">Submit</Button>
+            <Button type="submit" className="shad-button_primary whitespace-nowrap" disabled={isLoadingCreate || isLoadingUpdate}>{isLoadingCreate || isLoadingUpdate && 'Loading...'} {action}</Button>
 
         </div>
       </form>
