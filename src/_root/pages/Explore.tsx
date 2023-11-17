b4798125dcff5422eb5d2875941e4e31477e1bb7
manuel-heav <manuelheav32@gmail.com>
2023-11-17 16:39:10 +0300

    working on explore page

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -1,8 +1,15 @@
+import GridPostList from '@/components/shared/GridPostList';
+import SearchResults from '@/components/shared/SearchResults';
 import { Input } from '@/components/ui/input'
 import React, { useState } from 'react'
 
 const Explore = () => {
-  const [searchValue, setSearchValue] = useState('')
+  const [searchValue, setSearchValue] = useState('');
+
+  // const posts = []
+
+  // const shouldShowSearchResults = searchValue !== '';
+  // const shouldShowPosts = !shouldShowSearchResults && posts?.pages.every((item) => item.documents.length === 0)
   return (
     <div className='explore-container'>
       <div className="explore-inner_container">
@@ -21,6 +28,18 @@ const Explore = () => {
           <img src="/assets/icons/filter.svg" alt="" width={20} height={20}/>
         </div>
       </div>
+
+      {/* <div className="flex flex-wrap gap-9 w-full max-w-5xl">
+      {shouldShowSearchResults ? (
+        <SearchResults 
+        
+        />
+      ) : shouldShowPosts ? (
+        <p className='text-light-4 mt-10 text-center w-full'>End of Posts</p>
+      ) : posts.pages.map((item, index) => (
+        <GridPostList key={`page-${index}`} posts={item.documents}/>
+      ))}
+      </div> */}
     </div>
   )
 }
